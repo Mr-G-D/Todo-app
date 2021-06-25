347cec7f04db9cf3bafeba2ac026da4ce0fe3d97
Mr-G-D <daredinesh00@gmail.com>
2021-06-25 16:03:13 +0530

    components added

---
+++ components/Task.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const Task = () => {
+  return (
+    <View style={styles.items}>
+      <View style={styles.itemLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <Text style={styles.itemText}>This is a Task</Text>
+      </View>
+      <Text style={styles.circular}></Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  items: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {},
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#55bcf6",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
+export default Task;
